a36eee0807bc2bb54eadbd136695d8bb7453b067
hummingbird <songxianduo@163.com>
2021-09-24 12:47:30 +0800

    third commit

--- src/main/java/com/hummingbird/gitTest.java
+++ src/main/java/com/hummingbird/gitTest.java
@@ -4,5 +4,7 @@ public class gitTest {
 
     public static void main(String[] args){
         System.out.println("Hello git!");
+        System.out.println("second test");
+        System.out.println("third commit");
     }
 }
