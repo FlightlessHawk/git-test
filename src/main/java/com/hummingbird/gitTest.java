f899d481ccdd2a4c21d365b79764a71e84121c1e
hummingbird <songxianduo@163.com>
2021-09-25 13:22:03 +0800

    push commit

--- src/main/java/com/hummingbird/gitTest.java
+++ src/main/java/com/hummingbird/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
         System.out.println("i am hot-fix1");
         System.out.println("master test");
         System.out.println("hot-fix1 test");
+        System.out.println("idea推送代码到github");
     }
 }
